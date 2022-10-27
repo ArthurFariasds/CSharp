f4bf32989513b8fc2b9c54a3a503a106f8828539
ArthurFariasds <116211282+ArthurFariasds@users.noreply.github.com>
2022-10-27 11:50:56 -0300

    Add files via upload

---
+++ StringBuilder/StringBuilder/Entities/Comentario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StringBuilder.Entities
+{
+    internal class Comentario
+    {
+        public  string Texto { get; set; }
+
+        public Comentario()
+        {
+        }
+        public Comentario(string texto)
+        {
+            Texto = texto;
+        }
+    }
+}
---
+++ StringBuilder/StringBuilder/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using StringBuilder.Entities;
+
+namespace StringBuilder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Comentario coment1 = new Comentario("Tenha uma boa viagem!");
+            Comentario coment2 = new Comentario("Que sensacional!");
+
+            Post post = new Post(
+                DateTime.Parse("26/10/2022 10:17:33"),
+                "Viajando para o Egito",
+                "Estou indo visitar esse país maravilhoso",
+                12);
+
+            post.AddComentario(coment1);
+            post.AddComentario(coment2);
+
+            Console.WriteLine(post);
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
